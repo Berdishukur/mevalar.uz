5ca83fbe3d663835548b9ce9b0656f1420255521
Berdishukur <boboqulovberdishukur@gmail.com>
2024-01-12 20:45:33 +0500

    add

--- my_api/views.py
+++ my_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
