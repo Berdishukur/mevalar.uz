ce9fea57f220ae0f51119fa7a471ebdc3d0a882e
Berdishukur <boboqulovberdishukur@gmail.com>
2024-01-19 18:35:06 +0500

    add

--- food/urls.py
+++ food/urls.py
@@ -1,8 +1,13 @@
+from . import signals
+
 from django.urls import path
 from .views import *
+from .services import admin_replenish_stock
 urlpatterns = [
     path('',index,name="index"),
     path('home_page/',home_page,name="home_page"),
     path('order_page/',order_page,name="order_page"),
     path('order/',main_order,name="main_order"),
+    path('admin/replenish_stock/<int:product_id>/<int:amount>', admin_replenish_stock, name='admin_replenish_stock'),
+
 ]
