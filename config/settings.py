19dce1e54c503b256db65e5a81f08ad15b84b2d2
Berdishukur <boboqulovberdishukur@gmail.com>
2024-01-19 18:32:12 +0500

    add

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TELEGRAM_BOT_TOKEN="6262304531:AAHpL5H_qjji3lYhzSx0UTeEczcjmAYTWI4"
