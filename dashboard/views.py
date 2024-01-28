5f5a6dd49ca177c22ebf8be293d3ff0beb56eb61
Berdishukur <boboqulovberdishukur@gmail.com>
2024-01-28 22:27:25 +0500

    add

--- dashboard/views.py
+++ dashboard/views.py
@@ -26,7 +26,19 @@ def main_dashboard(request):
             }
         )
 
+    ctx = {
+        "counts": {
+            "categories":len(categories),
+            "products": len(products),
+            "customers": len(customers),
+            "orders": len(orders),
 
+        },
+        "categories_products": categories_products,
+        "table_list": table_list,
+
+    }
+    return render(request, 'dashboard/index.html',ctx)
 
 def login_page(request):
     if request.POST:
