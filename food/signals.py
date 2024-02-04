a5a5d0dca628b3752a15af8e0effe672740142fe
Berdishukur <boboqulovberdishukur@gmail.com>
2024-02-04 18:52:52 +0500

    add

--- food/signals.py
+++ food/signals.py
@@ -4,16 +4,3 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import Order,OrderProduct
 
-@receiver(post_save, sender=OrderProduct)
-def notify_admin(sender, instance, created, **kwargs):
-    if created:  # Check if a new record is created
-        token = settings.TELEGRAM_BOT_TOKEN
-        method = 'sendMessage'
-        message_text = f"Client: {instance.order.customer} \n Address: {instance.order.address} \n " \
-                       f" tel:{instance.order.customer.phone_number}\n Mahsulot: {instance.product.title} " \
-                       f"\nSoni:{instance.count}\nJami summa: {instance.price*instance.count}"
-
-        response = requests.post(
-            url=f'https://api.telegram.org/bot{token}/{method}',
-            data={'chat_id': 5259578459, 'text': message_text}
-        ).json()
