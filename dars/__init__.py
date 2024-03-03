2e361ddacc7b0c180d9bbeedeff322a08b883bfb
Berdishukur <boboqulovberdishukur@gmail.com>
2024-03-03 21:00:23 +0500

    add

--- dars/__init__.py
+++ dars/__init__.py
@@ -1,6 +1,9 @@
-son=int(input("son kiriting: "))
+
+
+a=int(input("a="))
+b=int(input("b="))
 sum=0
-for i in range(1,son+1):
-    if i%3==0 and i%9!=0:
-        sum+=i
+for i in range(a,b+1):
+    sum+=(i**2)
 print(sum)
+
