47cb47d96311e7fe6def881bea28f15a0d903e7c
Berdishukur <boboqulovberdishukur@gmail.com>
2024-03-04 16:00:34 +0500

    add

--- dars/__init__.py
+++ dars/__init__.py
@@ -1,9 +1,20 @@
 
+# a=int(input("a="))
+# sonlar=[]
+# sum=0
+# for i in range(2,a+1):
+#     count=0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#             count+=1
+#     if count>2:
+#         sum+=i
+# print(sum)
 
-a=int(input("a="))
-b=int(input("b="))
-sum=0
-for i in range(a,b+1):
-    sum+=(i**2)
-print(sum)
+# a=int(input("a="))
+# b=int(input("b="))
+# sum=0
+# for i in range(a,b+1):
+#     sum+=(i**2)
+# print(sum)
 
