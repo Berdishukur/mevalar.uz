659506e1d0d212c50cc77c7eda7458ab9ceba15f
Berdishukur <boboqulovberdishukur@gmail.com>
2024-03-18 13:59:28 +0500

    add

--- dars/__init__.py
+++ dars/__init__.py
@@ -1,12 +1,12 @@
-ages = [5, 12, 17, 18, 24, 32]
-def myFunc(x):
-  if x < 18:
-    return False
-  else:
-    return True
-
-adults = filter(myFunc, ages)
-print(list(adults))
+# ages = [5, 12, 17, 18, 24, 32]
+# def myFunc(x):
+#   if x < 18:
+#     return False
+#   else:
+#     return True
+#
+# adults = filter(myFunc, ages)
+# print(list(adults))
 # for x in adults:
 #   print(x)
 
