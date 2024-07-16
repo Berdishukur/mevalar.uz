e80a34184cf0fa3a7bf717d720d40335dba41b27
Berdishukur <boboqulovberdishukur@gmail.com>
2024-07-16 17:51:51 +0500

    add

--- food/models.py
+++ food/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(null=False, blank=False, max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
     title = models.CharField(null=False, blank=False, max_length=100)
