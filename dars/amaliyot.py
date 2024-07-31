b0bd348bea0eadd1e4b69728b0e55d6727ba429e
Berdishukur <boboqulovberdishukur@gmail.com>
2024-07-31 13:18:53 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,1 +1,9 @@
-dcdfcw
+def the_tower_of_hanoi(n,a,b,c):
+   if n==1:
+       print(f"{n}chi halqani {a} dan {c} ga joylashtir!")
+       return
+   the_tower_of_hanoi(n-1,a,b,c)
+   print(f"{n}chi halqani, {a},dan,{c}, ga joylashtir!")
+   the_tower_of_hanoi(n - 1, b,c,a)
+n=int(input("son kiriting>>>"))
+the_tower_of_hanoi(n, 'a', 'c' ,'b')
