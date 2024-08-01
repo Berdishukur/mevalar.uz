b75d304f8a1cc4d0717e7759266df33dc82faaae
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-01 19:27:11 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,11 +1,1 @@
-
-# 1. Stringni katta harflarga o'tkazadigan dekorator
-def uppercase_decorator(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        return result.upper() if isinstance(result, str) else result
-    return wrapper
-@uppercase_decorator
-def greet(name):
-    return f"Salom, {name}!"
-print(greet("Ali"))
+print(234)
