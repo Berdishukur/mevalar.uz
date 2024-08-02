7dc721c4befd71de2dd71fb5e7ac6802fd269e92
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-02 15:34:27 +0500

    add

--- dars/J1/dars_20.py
+++ dars/J1/dars_20.py
@@ -1,9 +1,37 @@
-i=1
-while i<=10:
-    if i%2==0:
-        print(i)
-    i+=1
 
+# i=1
+# while i<=7:
+#     hafta_kun = int(input("Hafta kuniga mos raqam kiriting : "))
+#     if hafta_kun==1:
+#         print("Bugun kun dushanba It ga bor.")
+#     elif hafta_kun==2:
+#         print("Bugun kun seshanba bilganingni qil.")
+#
+#     elif hafta_kun==3:
+#         print("Bugun kun Chorshanba It ga bor.")
+#
+#     elif hafta_kun==4:
+#         print("Bugun kun Payshnba bilganingni qil.")
+#
+#     elif hafta_kun==5:
+#         print("Bugun kun Juma It ga bor.")
+#
+#     elif hafta_kun==6:
+#         print("Bugun kun shanba  bilganingni qil.")
+#     elif hafta_kun==7:
+#         print("Bugun kun yakshanba damingni ol")
+#
+#     i+=1
+#
+#
+
+
+# i=1
+# while i<=10:
+#     if i%2==0:
+#         print(i)
+#     i+=1
+#
 
 
 
