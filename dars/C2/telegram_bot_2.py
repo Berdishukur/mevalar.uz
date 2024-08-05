ce8109009aa26d0783174dc0c1eb2e83032e6741
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-05 09:39:38 +0500

    add

--- dars/C2/telegram_bot_2.py
+++ dars/C2/telegram_bot_2.py
@@ -3,20 +3,17 @@ token="7393833956:AAGJvaWDD5l9i-rsWP8BTHOu8pDrZSz_6KY"
 
 def start_func(update, context):
     update.message.reply_text(text="Salom xush kelibsiz!!!")
+def photo_func(update,context):
+    update.message.reply_photo(photo="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg")
 
-def message_func(updet,context):
-    xabar=updet.message.text
+def message_func(update,context):
+    xabar=update.message.text
     if xabar.lower()=="salom":
-        updet.message.reply_text(text="Valaykum assalaom.")
+        update.message.reply_text(text="Valaykum assalaom.")
     elif xabar.lower()=="qalaysiz":
-        updet.message.reply_text(text="Men yaxshiman \n  O'zingiz ham yaxshimisiz?")
+        update.message.reply_text(text="Men yaxshiman \n  O'zingiz ham yaxshimisiz?")
     else:
-        updet.message.reply_text(text="Menu ni tanlang\n   \n /Photo \n /Audio")
-        menu=updet.message.text
-        if menu=="/Photo":
-            updet.message.reply_photo(photo="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg")
-        elif menu=="/Audio":
-            updet.message.reply_audio(audio="https://n1.quvonch.com/uploads/music/2024-04/xamdam-sobirov-malohat.mp3")
+        update.message.reply_text(text="Menu ni tanlang\n   \n /photo \n /Audio")
 
 
 
@@ -24,7 +21,10 @@ def main():
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start_func))
+    dispatcher.add_handler(CommandHandler("photo",photo_func))
     dispatcher.add_handler(MessageHandler(Filters.text,message_func))
+
+
     updater.start_polling()
     updater.idle()
 
