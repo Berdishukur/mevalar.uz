f7bb207607e7160edbbaacc7ae2cac3182b65ce0
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-06 15:48:35 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,19 +1,5 @@
- # import time
-#
-# def timer_decorator(func):
-#     def result_func(*x, **y):
-#         start_time = time.time()
-#         result = func(*x, *
-#         print(f"{func.__name__} ni bajarish uchun {end_time - start_time:.2f} sekund vaqt ketdi")
-#         return result
-#     return result_func
-#
-# @timer_decorator
-# def Calls_function():
-#     for x in range(100_000_000):
-#         if x==5_500_000:
-#             x=x*5
-#             print("Natija=",x)
-#
-# Calls_function()*y)
-#         end_time = time.time()
+class Oson1:
+    a=5
+    def print_a(cls):
+        print(cls.a)
+Oson1.print_a(Oson1)
