108fa036ed083757d0ef9d43f3460e17741a2650
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-06 11:15:14 +0500

    add

---
+++ dars/amaliyot.py
@@ -0,0 +1,19 @@
+import time
+
+def timer_decorator(func):
+    def result_func(*x, **y):
+        start_time = time.time()
+        result = func(*x, *
+        print(f"{func.__name__} ni bajarish uchun {end_time - start_time:.2f} sekund vaqt ketdi")
+        return result
+    return result_func
+
+@timer_decorator
+def Calls_function():
+    for x in range(100_000_000):
+        if x==5_500_000:
+            x=x*5
+            print("Natija=",x)
+
+Calls_function()*y)
+        end_time = time.time()
