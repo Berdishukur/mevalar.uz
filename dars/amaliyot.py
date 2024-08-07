799d5fa4f122e2d38be7aa23d7e28880fc1f2804
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-07 12:55:47 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,5 +1,28 @@
-class Oson1:
-    a=5
-    def print_a(cls):
-        print(cls.a)
-Oson1.print_a(Oson1)
+def expensiveProduct(products):
+    max_price= 0
+    max_product={}
+    for product in products:
+        if product["price"]> max_price:
+            max_price= product["price"]
+            max_product=product
+    return max_product["name"]
+
+qiymat=[
+  {
+    "name": "Iphone X",
+    "price": 60000000
+  },
+  {
+    "name": "Iphone 12",
+    "price": 1500
+  },
+  {
+    "name": "Samsung Note 9",
+    "price": 800
+  },
+  {
+    "name": "Samsung S10",
+    "price": 1100
+  },
+]
+print(expensiveProduct(qiymat))
