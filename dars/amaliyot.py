af851bbac9a4c1add0e1357e7762e985bdcb7eb7
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-09 15:57:04 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,28 +1,24 @@
-def expensiveProduct(products):
-    max_price= 0
-    max_product={}
-    for product in products:
-        if product["price"]> max_price:
-            max_price= product["price"]
-            max_product=product
-    return max_product["name"]
+import random
+x=random.randint(1,100)
+while True:
+    tanlov=int(input("Tanlavingizni kiriting : "))
+    if x==tanlov:
+        print("Siz yutdingiz!!!")
+        break
+    elif tanlov>=2*x:
+        print("Juda baland")
+    elif x<tanlov<2*x:
+        print("Ozgina balandroq")
+    elif tanlov<=int(x/2):
+        print("Juda past")
+    elif int(x/2)<tanlov < x:
+        print("Ozgina pastroq")
+    else:
+        print("Tanlovingiz xato")
 
-qiymat=[
-  {
-    "name": "Iphone X",
-    "price": 60000000
-  },
-  {
-    "name": "Iphone 12",
-    "price": 1500
-  },
-  {
-    "name": "Samsung Note 9",
-    "price": 800
-  },
-  {
-    "name": "Samsung S10",
-    "price": 1100
-  },
-]
-print(expensiveProduct(qiymat))
+
+
+
+# sonlar=[4,1,0,5,33,66,7,2]
+# sonlar.sort()
+# print(sonlar[-1])
