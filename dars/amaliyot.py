0a69dece9cf3a000262f0c33dded293b7fa8ade0
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-12 12:30:25 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,24 +1,29 @@
-import random
-x=random.randint(1,100)
-while True:
-    tanlov=int(input("Tanlavingizni kiriting : "))
-    if x==tanlov:
-        print("Siz yutdingiz!!!")
-        break
-    elif tanlov>=2*x:
-        print("Juda baland")
-    elif x<tanlov<2*x:
-        print("Ozgina balandroq")
-    elif tanlov<=int(x/2):
-        print("Juda past")
-    elif int(x/2)<tanlov < x:
-        print("Ozgina pastroq")
-    else:
-        print("Tanlovingiz xato")
+sonlar=[1,2,3,6,22,6,3,8,3,9,3]
+son=int(input("Sonni kiriting : "))
+a=sonlar.count(son)
+print(a)
 
-
-
-
-# sonlar=[4,1,0,5,33,66,7,2]
-# sonlar.sort()
-# print(sonlar[-1])
+# import random
+# x=random.randint(1,100)
+# while True:
+#     tanlov=int(input("Tanlavingizni kiriting : "))
+#     if x==tanlov:
+#         print("Siz yutdingiz!!!")
+#         break
+#     elif tanlov>=2*x:
+#         print("Juda baland")
+#     elif x<tanlov<2*x:
+#         print("Ozgina balandroq")
+#     elif tanlov<=int(x/2):
+#         print("Juda past")
+#     elif int(x/2)<tanlov < x:
+#         print("Ozgina pastroq")
+#     else:
+#         print("Tanlovingiz xato")
+#
+#
+#
+#
+# # sonlar=[4,1,0,5,33,66,7,2]
+# # sonlar.sort()
+# # print(sonlar[-1])
