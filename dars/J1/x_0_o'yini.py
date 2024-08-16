44141932a7e248946806decda6cdc684171d619d
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-16 16:04:39 +0500

    add

--- dars/J1/x_0_o'yini.py
+++ dars/J1/x_0_o'yini.py
@@ -12,7 +12,10 @@ print(f"""
 
 while True:
     x=int(input("X = "))
-    g[x-1]="X"
+    while g[x-1]=='X' or g[x-1]==0:
+        print("Bu katak band qaytadan kiriting: ")
+        x = int(input("X = "))
+    g[x - 1] = "X"
     print(f"""
                  {g[0]} | {g[1]} | {g[2]} 
                 -----------
@@ -21,3 +24,22 @@ while True:
                  {g[6]} | {g[7]} | {g[8]} 
     """
           )
+    if g[0]=="X" and g[1]=="X" and g[2]=="X" or g[3]=="X" and g[4]=="X" and g[5]=="X" or g[6]=="X" and g[7]=="X" and g[8]=="X" or g[0]=="X" and g[3]=="X" and g[6]=="X" or g[1]=="X" and g[4]=="X" and g[7] =="X" or g[2]=="X" and g[5]=="X" and g[8]=="X" or g[0]=="X" and g[4]=="X" and g[8]=="X" or g[2]=="X" and g[4]=="X" and g[6]=="X" :
+        print("X yudti. Xayr!!")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
