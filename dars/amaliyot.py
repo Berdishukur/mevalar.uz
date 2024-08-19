dc836df596fd9c24bd5b9bf81fa7ead4e84eb7ce
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-19 14:23:35 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,28 +1,74 @@
-yosh=int(input("Yoshingizni kiriting : "))
-if yosh>=60:
-    print("Siz pensiya yoshidasiz.")
-elif 18<yosh<60:
-    print("Siz ishlaydigan yoshdasiz")
-elif 7<=yosh<=18:
-    print("Siz maktab yoshidasiz")
-elif 2<yosh<7:
-    print("Siz bog'cha yoshidasiz")
+#     1. A soni berilgan. Uni musbat yoki manfiy ekanligini aniqlang
+
+# A=8
+# if A>0:
+#     print("Musbat")
+# elif A<0:
+#     print("Manfiy")
+# else:
+#     print("Nol")
+
+# 11. A, B va C sonlari berilgan. Ulardan nechtasi musbat ekanligni aniqlang.
+# A=int(input(" A = "))
+# B=int(input(" B = "))
+# C=int(input(" C = "))
+#
+# if A>0 and B>0 and C>0:
+#     print(3," ta musbat")
+# elif A>0 and B>0 and  C<0 or A>0 and B<0 and C>0 or A<0 and B>0 and C>0:
+#     print(2,"ta musbat")
+#
+# elif A>0 or B>0 or C>0:
+#     print(1," ta musbat")
+# else:
+#     print("Musbat sonlar yo'q ")
+
+
+# 12. A, B va C sonlari berilgan. Ulardan ikkita  eng katta sonlarni yig’indisini aniqlang.
+#
+# A=int(input(" A = "))
+# B=int(input(" B = "))
+# C=int(input(" C = "))
+# if C<B<A or C<A<B:
+#     print(A+B)
+# elif B<C<A or B<A<C:
+#     print(C+A)
+# elif A<C<B or A<B<C:
+#     print(B+C)
+# else:
+#     print("2 ta katta son yo'q")
+
+# 14. A va B sonlari berilgan. A va B ni , yani ikkalasi ham tog’ sonlar ekanligini
+# aniqlang
+
+
+# A=int(input(" A = "))
+# B=int(input(" B = "))
+#
+# if A%2==1 and B%2==1:
+#     print(True)
+# else:
+#     print(False)
+
+# 16. A, B va C sonlari berilgan. A, B va C sonlarini musbatmi shuni aniqlang.
+
+# A=int(input(" A = "))
+# B=int(input(" B = "))
+# C=int(input(" C = "))
+#
+# if A>0 and B>0 and C>0:
+#     print("Musbat")
+# else:
+#     print("Musbat emas")
+
+# 17. A soni berilgan. Uni juft va 2 xonalik son ekanligni aniqlang.
+
+A=int(input(" A = "))
+
+if A%2==0 and 9<A<100:
+    print(True)
 else:
-    print("Siz ona qaramog'idasiz")
-
-
-# if yosh>=60:
-#     print("Siz pensiya olishingiz mumukun")
-# else:
-#     print("Sizning yoshingiz yetmadi")
-
-
-# son=int(input("Sonni kiriting : "))
-#
-# if son%2==0:
-#     print(son+2)
-# else:
-#     print(son-2)
+    print(False)
 
 
 
@@ -36,27 +82,3 @@ else:
 
 
 
-# import random
-# x=random.randint(1,100)
-# while True:
-#     tanlov=int(input("Tanlavingizni kiriting : "))
-#     if x==tanlov:
-#         print("Siz yutdingiz!!!")
-#         break
-#     elif tanlov>=2*x:
-#         print("Juda baland")
-#     elif x<tanlov<2*x:
-#         print("Ozgina balandroq")
-#     elif tanlov<=int(x/2):
-#         print("Juda past")
-#     elif int(x/2)<tanlov < x:
-#         print("Ozgina pastroq")
-#     else:
-#         print("Tanlovingiz xato")
-#
-#
-#
-#
-# # sonlar=[4,1,0,5,33,66,7,2]
-# # sonlar.sort()
-# # print(sonlar[-1])
