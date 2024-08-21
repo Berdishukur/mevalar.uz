14d73274f7b2b814b0485394128f063f180b2527
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-21 17:53:14 +0500

    add

--- dars/J1/x_0_o'yini.py
+++ dars/J1/x_0_o'yini.py
@@ -1,37 +1,13 @@
-g=[1,2,3,4,5,6,7,8,9]
+g=["🍎","🍎","🍎","🍎","🍎","🍎","🍎","🍎","🍎"]
 
-
-print(f"""
-             {g[0]} | {g[1]} | {g[2]} 
-            -----------
-             {g[3]} | {g[4]} | {g[5]}
-            -----------
-             {g[6]} | {g[7]} | {g[8]} 
-"""
-      )
+import  random
 
 while True:
-    x=int(input("X = "))
-    while g[x-1]=='X' or g[x-1]==0:
-        print("Bu katak band qaytadan kiriting: ")
-        x = int(input("X = "))
-    g[x - 1] = "X"
-    print(f"""
-                 {g[0]} | {g[1]} | {g[2]} 
-                -----------
-                 {g[3]} | {g[4]} | {g[5]}
-                -----------
-                 {g[6]} | {g[7]} | {g[8]} 
-    """
-          )
-    if g[0]=="X" and g[1]=="X" and g[2]=="X" or g[3]=="X" and g[4]=="X" and g[5]=="X" or g[6]=="X" and g[7]=="X" and g[8]=="X" or g[0]=="X" and g[3]=="X" and g[6]=="X" or g[1]=="X" and g[4]=="X" and g[7] =="X" or g[2]=="X" and g[5]=="X" and g[8]=="X" or g[0]=="X" and g[4]=="X" and g[8]=="X" or g[2]=="X" and g[4]=="X" and g[6]=="X" :
-        print("X yudti. Xayr!!")
-        break
-    o=int(input("O = "))
-    while g[o-1]=="X" or g[o-1]==0:
-        print("Bu katak band qaytadan kiriting: ")
-        o = int(input("O = "))
-    g[o-1]=0
+    o=random.randint(1,9)
+    print()
+    while g[o-1]=="🐢" or g[o-1]=="🐍":
+        o =random.randint(1,9)
+    g[o-1]="🐢"
     print(f"""
                     {g[0]} | {g[1]} | {g[2]} 
                    -----------
@@ -40,7 +16,27 @@ while True:
                     {g[6]} | {g[7]} | {g[8]} 
        """
           )
+    if g[0]==g[1]==g[2]=="🐢" or g[3]==g[4]== g[5]=="🐢" or g[6]==g[7]==g[8]=="🐢" or g[0]==g[3]==g[6]=="🐢" or g[1]== g[4]== g[7] =="🐢" or g[2]== g[5]== g[8] =="🐢" or g[0]== g[4]== g[8]=="🐢" or g[2]== g[4]== g[6]=="🐢":
+        print('0 yutdi! tamom')
+        break
 
+
+    x=int(input("X = "))
+    while g[x-1]=='🐢' or g[x-1]=="🐍":
+        print("Bu katak band qaytadan kiriting: ")
+        x = int(input("X = "))
+    g[x - 1] = "🐍"
+    print(f"""
+                 {g[0]} | {g[1]} | {g[2]} 
+                -----------
+                 {g[3]} | {g[4]} | {g[5]}
+                -----------
+                 {g[6]} | {g[7]} | {g[8]} 
+    """
+          )
+    if g[0]==g[1]==g[2]=="🐍" or g[3]==g[4]== g[5]=="🐍" or g[6]==g[7]==g[8]=="🐍" or g[0]==g[3]==g[6]=="🐍" or g[1]== g[4]== g[7] =="🐍" or g[2]== g[5]== g[8] =="🐍" or g[0]== g[4]== g[8]=="🐍" or g[2]== g[4]== g[6]=="🐍" :
+        print("X yudti. Xayr!!")
+        break
 
 
 
