f517c68497b61d230d59cd12cac2eae9134586d0
Berdishukur <boboqulovberdishukur@gmail.com>
2024-08-26 23:37:25 +0500

    add

---
+++ dars/C4/zoom_meeting_1.py
@@ -0,0 +1,25 @@
+yosh=22 # int
+miqdor=5.7 # float
+matn="Salom dunyo" #str
+jinsi=True #bool
+
+
+
+# ism="Ali"
+# print(ism)
+
+
+
+# print("salom")
+
+# list1=[1,2.5,True,"Salom"]
+# tuple1=(1,2.5,True,"Salom")
+
+
+# yosh=int(input("Yoshingizni kiriting : "))
+# print(2024-yosh)
+
+
+# matn='Uzbekiston kelajagi "buyuk" davlat!'
+# ism="G'anisher"
+# print(ism)
