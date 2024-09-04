bc63d568d7adf785151760429b369d851c655f20
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-04 15:40:26 +0500

    add

---
+++ dars/C4/zoom meeting_2.py
@@ -0,0 +1,28 @@
+# sonlar=[3,1,5,22,7,33,11,99,0]
+# print(sonlar[2:7])
+
+
+
+matn="sAlom xush kelibsiz bizning dasturga"
+print(matn.upper().count("A"))
+# print(matn.index('xush'))
+#
+#
+# print(matn.center(60))
+
+
+# matn3="""
+#
+#
+# sqhhbqde
+# qwehbckwdb
+#
+# """
+# print(matn3)
+
+
+# bozorlik1=['olma','anor','nok']
+# bozorlik2=['kitob','ruchka']
+#
+# bozorlik1.extend(bozorlik2)
+# print(bozorlik1)
---
+++ dars/J1/milloner_o'yini.py
@@ -0,0 +1,4 @@
+
+while True:
+    n=int(input("Kim millioner bo'lishni xohlaydi ? \n    1 ==>> o'ynash\n    2 ==>> reyting\n    0 ==>> Dasturdan chiqish\n    >>>>>..."))
+    if n==1
