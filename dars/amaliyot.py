456b9c536143d4776305304cf76126e35739b895
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-06 16:58:33 +0500

    add

--- dars/amaliyot.py
+++ dars/amaliyot.py
@@ -1,111 +1,168 @@
-# """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import json
-
-with open("test.json", 'r') as f:
-    json_to_python = json.load(f)
-
-for savol in json_to_python:
-    print(savol["question"])
-    javoblar = savol['answers']
-
-    variantlar = ['A', 'B', 'C', 'D']
-    for i, javob in enumerate(javoblar):
-        print(f"{variantlar[i]}) {javob['answers']}")
-
-    tanlov = input((f"A){javoblar[0]}\nB){javoblar[1]}\nC{javoblar[2]}\nD){javoblar[3]}\n   >>>>> "))
-    tanlangan_javob = javoblar[variantlar.index(tanlov.upper())]
-    if tanlov == "A" and javoblar[0][" isTrue"] == True:
-        print("Sizning javobingiz to'g'ri")
-    else:
-        print("Siz xato javob qaytardingiz")
-
-
-
-
-
-
-# foydalanuvchidan qiziqishlari so'ralsin.
-#
-# Agar kitob yoki kutubxona qiziqishlari orasida bo'lsa, qanday kitoblar yoqishi so'ralsin.
-#     agar detektiv so'zini ishlatsa, shaytanat kitobi haqidagi fikri so'ralsin.
-#     agar diniy kitoblarga qiziqsa "Hadis va Hayot" kitoblar to'plamini sovg'a qilamiz deyilsin.
-#
-# Agar sport so'zi qiziqishlari orasida bo'lsa, qaysi sport turiga qiziqishi so'ralsin,
-#    agar futbol sport turlari orasida bo'lsa, qaysi komandani yoqtirishi so'ralsin.
-#         agar real yoki barsa komandasini yozsa, el-classicoga chipta sovg'a qilamiz deyilsin
-# """
-#
-# qiziqish=input("Nimga qiziqasiz ? ")
-#
-# if "kitob" in qiziqish or "kutubxona" in qiziqish:
-#     kitob=input("Qanday kitoblarga qiziqasiz ? ")
-#     if "detektiv" in kitob:
-#         fikir=input("Shaytanat kitobi haqida fikringiz qanday ? ")
-#     elif "diniy" in kitob:
-#         print("Hadis va Hayot kitoblari sizga sovg'a")
-
-
-# elif "sport" in qiziqish:
+# if 2>3:
 #     pass
 # else:
-#     print("Sizdagi qiziqishlar haqida bizda malumot yo'q")
+#     print("csdc")
 #
 
+# rang =["Qora","Oq","Yashil"]
+# mashena=["Nexia","lasetti","gentira"]
+# for i in rang:
+#     for j in mashena:
+#         print(i,j)
+
+
+
+# for i in range(1,10):
+#     print(i)
+#     if i==3:
+#         continue
+# else:
+#     print("Sikl tugadi")
+
+# for i in range(7):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(1,11):
+#     if i==7:
+#         continue
+#     print(i)
 
 
 
 
 
 
-
-
-
-# N=int(input("N  = "))
-# r1=str(N)[0]
-# r2=str(N)[1]
-# r3=str(N)[2]
-# r4=str(N)[3]
+# for i in "dastur":
 #
-# if r1<r2<r3<r4:
-#     print("o'suvchi")
-# elif r1>r2>r3>r4:
-#     print("kamayuvchi")
-# else:
-#     print("tartibsiz")
+#     if i == 's':
+#         break
+#     print(i)
+# for i in range(1,100):
+#     print(i)
+#     if i==10:
+#         break
 
 
-# N=int(input(" N= "))
-# if N%2==0:
-#     print(N**2)
-# else:
-#     print(N**3)
+
+# for i in "S alom":
+#     print(i)
+
+# meva = ["olma", "anor", "banan"]
+# for i in meva:
+#     print(i)
 
 
-# fuqoralik=input("Millatingizni kiriting : ")
-# yosh=int(input("Yoshingizni kiriting : "))
+
+
+
+
+
+
+
+
+
+
+
 #
-# if "uzb" in fuqoralik.lower() and yosh>=18:
-#     print('sizda saylash huquqi bor')
-# else:
-#     print("sizda saylash huquqi yo'q")
-
-
-
+#
+#
+#
+#
+#
+#
+#
+# import json
+#
+# with open("test.json", 'r') as f:
+#     json_to_python = json.load(f)
+#
+# for savol in json_to_python:
+#     print(savol["question"])
+#     javoblar = savol['answers']
+#
+#     variantlar = ['A', 'B', 'C', 'D']
+#     for i, javob in enumerate(javoblar):
+#         print(f"{variantlar[i]}) {javob['answers']}")
+#
+#     tanlov = input((f"A){javoblar[0]}\nB){javoblar[1]}\nC{javoblar[2]}\nD){javoblar[3]}\n   >>>>> "))
+#     tanlangan_javob = javoblar[variantlar.index(tanlov.upper())]
+#     if tanlov == "A" and javoblar[0][" isTrue"] == True:
+#         print("Sizning javobingiz to'g'ri")
+#     else:
+#         print("Siz xato javob qaytardingiz")
+#
+#
+#
+#
+#
+#
+# # foydalanuvchidan qiziqishlari so'ralsin.
+# #
+# # Agar kitob yoki kutubxona qiziqishlari orasida bo'lsa, qanday kitoblar yoqishi so'ralsin.
+# #     agar detektiv so'zini ishlatsa, shaytanat kitobi haqidagi fikri so'ralsin.
+# #     agar diniy kitoblarga qiziqsa "Hadis va Hayot" kitoblar to'plamini sovg'a qilamiz deyilsin.
+# #
+# # Agar sport so'zi qiziqishlari orasida bo'lsa, qaysi sport turiga qiziqishi so'ralsin,
+# #    agar futbol sport turlari orasida bo'lsa, qaysi komandani yoqtirishi so'ralsin.
+# #         agar real yoki barsa komandasini yozsa, el-classicoga chipta sovg'a qilamiz deyilsin
+# # """
+# #
+# # qiziqish=input("Nimga qiziqasiz ? ")
+# #
+# # if "kitob" in qiziqish or "kutubxona" in qiziqish:
+# #     kitob=input("Qanday kitoblarga qiziqasiz ? ")
+# #     if "detektiv" in kitob:
+# #         fikir=input("Shaytanat kitobi haqida fikringiz qanday ? ")
+# #     elif "diniy" in kitob:
+# #         print("Hadis va Hayot kitoblari sizga sovg'a")
+#
+#
+# # elif "sport" in qiziqish:
+# #     pass
+# # else:
+# #     print("Sizdagi qiziqishlar haqida bizda malumot yo'q")
+# #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # N=int(input("N  = "))
+# # r1=str(N)[0]
+# # r2=str(N)[1]
+# # r3=str(N)[2]
+# # r4=str(N)[3]
+# #
+# # if r1<r2<r3<r4:
+# #     print("o'suvchi")
+# # elif r1>r2>r3>r4:
+# #     print("kamayuvchi")
+# # else:
+# #     print("tartibsiz")
+#
+#
+# # N=int(input(" N= "))
+# # if N%2==0:
+# #     print(N**2)
+# # else:
+# #     print(N**3)
+#
+#
+# # fuqoralik=input("Millatingizni kiriting : ")
+# # yosh=int(input("Yoshingizni kiriting : "))
+# #
+# # if "uzb" in fuqoralik.lower() and yosh>=18:
+# #     print('sizda saylash huquqi bor')
+# # else:
+# #     print("sizda saylash huquqi yo'q")
+#
+#
+#
