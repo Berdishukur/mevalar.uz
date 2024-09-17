0c28750754cd5c0c9164334750e0fed26cf3cbaf
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-17 14:42:10 +0500

    add

--- dars/J1/dars_41.py
+++ dars/J1/dars_41.py
@@ -10,17 +10,37 @@
 
 
 
-def daraja(n):
-    return n **2
+# def daraja(n):
+#     return n **2
+#
+# numbers = [1, 2, 3, 4]
+# result = map(daraja, numbers)
+# print(list(result))
+#
+# l1=[1,2,3,4,5]
+# l2=[10,20,30,40,50]
+# nateja=map(lambda x,y: x+y,l1,l2)
+# print(list(nateja))
 
-numbers = [1, 2, 3, 4]
-result = map(daraja, numbers)
-print(list(result))
 
-l1=[1,2,3,4,5]
-l2=[10,20,30,40,50]
-nateja=map(lambda x,y: x+y,l1,l2)
+sonlar=[1,2,3,4,5,6,7,8,9]
+
+def fun(n):
+    if n%2==0:
+        return n**2
+    else:
+        return n**3
+nateja=map(fun,sonlar)
 print(list(nateja))
+
+
+
+
+
+
+
+
+
 
 
 
