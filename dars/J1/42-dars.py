dc322637695c36c8864f25b8454ed10d053375ba
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-19 14:23:12 +0500

    add

---
+++ dars/J1/42-dars.py
@@ -0,0 +1,28 @@
+class Oson1:
+    a=int(input(" a ni kiriting : "))
+    def output_a(self):
+        print(self.a)
+
+Oson1.output_a(Oson1)
+
+
+
+
+
+
+
+
+
+# class Person:
+#     bosh=1
+#     def __init__(self,name="Alisa",age=22,gender=True):
+#         self.name=name
+#         self.age=age
+#         self.gender=gender
+#     def speak(self,word):
+#         print(f"{self.name} {word} ni gapirdi")
+#
+# person5=Person(name="Lobar",gender=False)
+# person5.speak("Hello")
+# # person1=Person("Ali",25,True)
+# person2=Person("Zulayho",22,False)
