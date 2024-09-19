5dd0034fc027d11d72aac734a3bc2d4b067cff92
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-19 14:23:26 +0500

    add

---
+++ dars/B3/zoom_meeting_11.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+# class Oson5:
+#     a=int(input(" A = "))
+#     b=int(input(" B = "))
+#     def daraja(self):
+#         print("nateja = ",self.a**self.b)
+# oson5=Oson5()
+# oson5.daraja()
