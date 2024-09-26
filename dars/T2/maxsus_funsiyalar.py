905cf8466f8190e86289785cb68ef75b5324e159
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-26 18:06:36 +0500

    add

--- dars/T2/maxsus_funsiyalar.py
+++ dars/T2/maxsus_funsiyalar.py
@@ -1,4 +1,36 @@
 
-mevalar=["Olma","Olcha","Nok","Anor","Banan"]
-My_mevalar=[meva for meva in  mevalar if len(meva)>3 and meva[0]=="O"]
-print(My_mevalar)
+
+# def yigindi(a,b):
+#     return a+b
+# print(yigindi(2,6))
+
+# yigindi=lambda a,b:a+b
+# print(yigindi(4,7))
+# indx=lambda my_list:my_list[0]
+# print(indx([2,4,8]))
+
+# n=int(input(" N = "))
+# kv_sonlar=[i for i in range(1,n) (lambda i:i**2)]
+# print(kv_sonlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# mevalar=["Olma","Olcha","Nok","Anor","Banan"]
+# My_mevalar=[meva for meva in  mevalar if len(meva)>3 and meva[0]=="O"]
+# print(My_mevalar)
