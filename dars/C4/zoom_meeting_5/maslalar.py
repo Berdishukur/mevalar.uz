daacecb4c5d2ae4e31fd668862bc2e6939ea25c5
Berdishukur <boboqulovberdishukur@gmail.com>
2024-09-28 13:57:49 +0500

    add

--- dars/C4/zoom_meeting_5/maslalar.py
+++ dars/C4/zoom_meeting_5/maslalar.py
@@ -1,10 +1,28 @@
+class Student:
+    def __init__(self,name,age,gender):
+        self.name=name
+        self.age=age
+        self.gender=gender
+        print("Work init")
 
-class MyClass10:
-    nums=[1,2,3,4,5,6,7,8,9,10]
-    sum1=0
-    def devide(self,d):
-        for i in self.nums:
-            if i%d==0:
-                self.sum1+=int(i)
-        return self.sum1
-print(MyClass10.devide(MyClass10,5))
+talaba1=Student("Ali",25,"Erkak")
+print(talaba1.name)
+
+
+
+
+
+
+
+
+
+
+# class MyClass10:
+#     nums=[1,2,3,4,5,6,7,8,9,10]
+#     sum1=0
+#     def devide(self,d):
+#         for i in self.nums:
+#             if i%d==0:
+#                 self.sum1+=int(i)
+#         return self.sum1
+# print(MyClass10.devide(MyClass10,5))
