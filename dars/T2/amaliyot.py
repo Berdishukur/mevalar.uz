2568c4911b319f9bccf8f305b646bbd6ced12360
Berdishukur <boboqulovberdishukur@gmail.com>
2024-10-01 14:21:39 +0500

    add

--- dars/T2/amaliyot.py
+++ dars/T2/amaliyot.py
@@ -1,16 +1,5 @@
 
 s="vx"
 
-try:
-    print(11/0)
-except NameError as e:
-    print(f"{e} Sizda xatolik mavjud")
-except ZeroDivisionError as e:
-    print(f"{e} Sizda xatolik mavjud")
-
-except:
-    print("Siz qandaydir xatolik mavjud")
-finally:
-    print("Siz dasturni oxiriga yetib keldingiz.")
 
 print(12345)
