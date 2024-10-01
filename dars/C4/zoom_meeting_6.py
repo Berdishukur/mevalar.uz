f023ccd440c34c9670cb7e3f64b6d32ebcc4a14f
Berdishukur <boboqulovberdishukur@gmail.com>
2024-10-01 21:32:49 +0500

    add

---
+++ dars/C4/zoom_meeting_6.py
@@ -0,0 +1,32 @@
+A=int(input("A  =  "))
+N=int(input("N  =  "))
+for i in range(1,N):
+    print(f"{A} ** {i}  = {A**i}")
+
+
+
+# def F(x):
+#     z=5
+#     def G(y):
+#         return (x+z*y)
+#     return G
+# print(F(2)(7))
+#
+# nateja=F(4)(8)
+# print(nateja)
+
+
+
+
+
+
+
+
+
+
+
+# my_dict={1:"Bir",2:"Ikki",3:"Uch"}
+# # up_dict={1:"One"}
+# # my_dict.update(up_dict)
+# my_dict[1]="One"
+# print(my_dict)
